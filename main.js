1b029208689a834b6c9695df99e35bc73c258cac
sgujuluva <sanghee04@gmail.com>
2022-06-04 19:03:49 +0200

    active class added in each image through js

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 // selecting the images
 let images = document.querySelectorAll(".images");
+images.forEach(image => {
+    image.addEventListener("click",() => {
+        removeActiveClasses();
+        image.classList.add("active");
+    })
+})
+function removeActiveClasses(){
+    images.forEach(item => {
+        item.classList.remove("active");
+    })
+}
