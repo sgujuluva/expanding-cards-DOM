d16325e53f7918b0caeec715903f9eb9c8207168
sgujuluva <sanghee04@gmail.com>
2022-06-04 19:09:18 +0200

    commented

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 // selecting the images
 let images = document.querySelectorAll(".images");
+// adding active class to each image when clicked
 images.forEach(image => {
     image.addEventListener("click",() => {
-        removeActiveClasses();
+        removeActiveClasses(); // remove the old active when new image is clicked
         image.classList.add("active");
     })
-})
+});
+// for removing the active
 function removeActiveClasses(){
     images.forEach(item => {
         item.classList.remove("active");
